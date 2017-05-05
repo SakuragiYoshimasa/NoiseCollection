41419c7c52d18d8d489894ef9d1528217307f7ea
Yoshimasa Sakuragi <ysrhsp@outlook.com>
2017-05-05 15:08:54 +0900

    base done

--- Assets/Scripts/NoiseViewer.cs
+++ Assets/Scripts/NoiseViewer.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using ExperimentUtilities;
-
 
 //1d->1d: Only graph
 //2d->1d:
@@ -10,129 +10,172 @@ using ExperimentUtilities;
 //3d->3d:
 //4d->1d:
 //4d->3d:
-[RequireComponent(typeof(MeshRenderer))]
-public class NoiseViewer : MonoBehaviour {
 
-	#region Parameters
-	[Header("BufferSize")]
-	[SerializeField, Range(1, 2048)] int size_x = 2048;
-	[Tooltip("User for Trailer")]
-	[SerializeField, Range(1, 2048)] int size_y = 1; 
-	[SerializeField] float Z;
-	#endregion
+namespace NoiseCollection {
+	[RequireComponent(typeof(MeshRenderer))]
+	public class NoiseViewer : MonoBehaviour {
 
-	#region ShaderAndMat
-	[Header("Shader")]
-	[SerializeField] Shader _kernel;
-	[SerializeField] Shader _surface;
-	Material _kernelMat;
-	Material _surfaceMat;
-	[SerializeField] Material _testMat;
-	Mesh _mesh;
+		#region Parameters
+		[Header("BufferSize")]
+		[SerializeField, Range(1, 2048)] int size_x = 2048;
+		[Tooltip("User for Trailer")]
+		[SerializeField, Range(1, 2048)] int size_y = 1; 
+		#endregion
 
-	enum KernelPass {
-		InitPosition, UpdatePosition,
-		InitVelocity, UpdateVelocity
-	}
-	#endregion
+		#region ShaderAndMat
+		[Header("Shader")]
+		[SerializeField] Shader _kernel;
+		[SerializeField] Shader _surface;
+		Material _kernelMat;
+		Material _surfaceMat;
 
-	#region Buffer
-	[SerializeField] RenderTexture positionBufferDist;
-	[SerializeField] RenderTexture positionBufferSource;
-	[SerializeField] RenderTexture velocityBufferDist;
-	[SerializeField] RenderTexture velocityBufferSource;
-	#endregion
-
-	#region Status
-	[Header("Status")]
-	[SerializeField] bool _asVelocity;
-
-	enum NoiseType {
-		Parlin2D, Parlin3D, Parlin4D,
-		Simplex2D, Simplex3D, Simplex4D, 
-		Curl2D, Curl3D, Curl4D
-	}
-
-	[SerializeField] NoiseType _noise = NoiseType.Parlin2D;
-	#endregion
-
-	#region Update Buffer and Render
-	void KernelUpdate(){
-		//Set Params
-		_kernelMat.SetFloat("_Z", Z);
-		_kernelMat.SetInt("_AsVelocity", 0);
-		_kernelMat.SetTexture("_PositionTex", positionBufferSource);
-		//Update
-		Graphics.Blit(null, positionBufferDist, _kernelMat, (int)KernelPass.UpdatePosition);
-		if(_asVelocity && velocityBufferDist != null) {
-			Graphics.Blit(null, velocityBufferDist, _kernelMat, (int)KernelPass.UpdateVelocity);
+		enum KernelPass {
+			InitPosition, UpdatePosition,
+			InitVelocity, UpdateVelocity
 		}
-		
-		SwapBuffer();
-	}
+		#endregion
 
-	void SwapBuffer(){
-		RenderTexture temp = positionBufferDist;
-		positionBufferDist = positionBufferSource;
-		positionBufferSource = temp;
-		temp = velocityBufferDist;
-		velocityBufferDist = velocityBufferSource;
-		velocityBufferSource = temp;
-	}
+		#region Render Resource
+		Mesh _mesh;
+		[SerializeField] RawImage _textureCanvas;
+		#endregion
 
-	void RenderUpdate(){
-		//Set Params
-		_surfaceMat.SetTexture("_PositionTex", positionBufferSource);
-		Graphics.DrawMesh (_mesh, transform.localToWorldMatrix, _surfaceMat, this.gameObject.layer);
-		//Graphics.DrawMesh (_mesh, transform.localToWorldMatrix, _testMat, this.gameObject.layer);
-	}
-	#endregion
+		#region Buffer
+		RenderTexture positionBufferDist;
+		RenderTexture positionBufferSource;
+		RenderTexture velocityBufferDist;
+		RenderTexture velocityBufferSource;
+		#endregion
 
-	#region Main Cycle
-	void Awake () {
-		InitMaterials();
-		InitBuffers(_asVelocity);
-		InitMesh();
-	}
+		#region Status
+		[Header("Status")]
+		[SerializeField] bool _asVelocity;
+		[SerializeField] bool _threeD;
 
-	void LateUpdate () {
-		KernelUpdate();
-		RenderUpdate();
-	}
-	#endregion
+		enum NoiseType {
+			ClassicPerlin, PeriodicPerlin, Simplex, SimplexGradN, SimplexGradA
+		}
 
-	#region Init
-	void InitBuffers(bool asVelocity){
-		positionBufferDist = Buffer.CreateSquereRTBuffer(size_x, size_y);
-		positionBufferSource = Buffer.CreateSquereRTBuffer(size_x, size_y);
-		if(!asVelocity) return;
-		velocityBufferDist = Buffer.CreateSquereRTBuffer(size_x, size_y);
-		velocityBufferSource = Buffer.CreateSquereRTBuffer(size_x, size_y);
-	}
+		[SerializeField] NoiseType _noise = NoiseType.ClassicPerlin;
 
-	void InitMaterials(){
-		_kernelMat = MaterialFuncs.CreateMaterial(_kernel);
-		_surfaceMat = MaterialFuncs.CreateMaterial(_surface);
+		enum ViewType {
+			Texture, Mesh
+		}
+		[SerializeField] ViewType _viewType = ViewType.Texture;
+		#endregion
 
-		Graphics.Blit(null, positionBufferDist, _kernelMat, (int)KernelPass.InitPosition);
-		Graphics.Blit(null, velocityBufferDist, _kernelMat, (int)KernelPass.InitVelocity);
-	}
+		#region Update Buffer and Render
+		void KernelUpdate(){
+			//Set Params
+			_kernelMat.SetTexture("_PositionTex", positionBufferSource);
+			//Update
+			Graphics.Blit(null, positionBufferDist, _kernelMat, (int)KernelPass.UpdatePosition);
+			if(_asVelocity && velocityBufferDist != null) {
+				Graphics.Blit(null, velocityBufferDist, _kernelMat, (int)KernelPass.UpdateVelocity);
+			}
+			
+			SwapBuffer();
+		}
 
-	void InitMesh(){
+		void SwapBuffer(){
+			RenderTexture temp = positionBufferDist;
+			positionBufferDist = positionBufferSource;
+			positionBufferSource = temp;
+			temp = velocityBufferDist;
+			velocityBufferDist = velocityBufferSource;
+			velocityBufferSource = temp;
+		}
 
-		_mesh = new Mesh();
-		
-		Vector3[] vertices = new Vector3[size_x * size_y];
-		int[] indices = new int[size_x * size_y];
-		for(int i = 0; i < size_x; i++){
-			for(int j = 0; j < size_y; j++){
-				vertices[i * size_y + j] = new Vector3((float)i / (float)size_x + 0.5f, (float)j / (float)size_y + 0.5f, 1.0f);
-				indices[i * size_y + j] = i * size_y + j;
+		void RenderUpdate(){
+			if(_viewType == ViewType.Mesh){
+				
+				_textureCanvas.gameObject.SetActive(false);
+				_surfaceMat.SetTexture("_PositionTex", positionBufferSource);
+				Graphics.DrawMesh (_mesh, transform.localToWorldMatrix, _surfaceMat, this.gameObject.layer);
+			}else if(_viewType == ViewType.Texture){
+				_textureCanvas.gameObject.SetActive(true);
+				_textureCanvas.texture = positionBufferSource;
 			}
 		}
 
-		_mesh.vertices = vertices;
-		_mesh.SetIndices(indices, MeshTopology.Points, 0);
+		void UpdateShaderKeywords(){
+			_kernelMat.shaderKeywords = null;
+
+			switch(_noise){
+				case NoiseType.ClassicPerlin:
+					_kernelMat.EnableKeyword("CNOISE");
+					break;
+				case NoiseType.PeriodicPerlin:
+					_kernelMat.EnableKeyword("PNOISE");
+					break;
+				case NoiseType.Simplex:
+					_kernelMat.EnableKeyword("SNOISE");
+					break;
+				case NoiseType.SimplexGradN:
+					_kernelMat.EnableKeyword("SNOISE_NGRAD");
+					break;
+				case NoiseType.SimplexGradA:
+					_kernelMat.EnableKeyword("SNOISE_AGRAD");
+					break;
+				default: break;
+			}
+
+			if(_threeD) _kernelMat.EnableKeyword("THREED");
+			if(_asVelocity) _kernelMat.EnableKeyword("ASVELOCITY");
+		}
+
+		#endregion
+
+		#region Main Cycle
+		void Awake () {
+			InitMaterials();
+			InitBuffers(_asVelocity);
+			InitMesh();
+		}
+
+		void Update(){
+			UpdateShaderKeywords();
+		}
+
+		void LateUpdate () {
+			KernelUpdate();
+			RenderUpdate();
+		}
+		#endregion
+
+		#region Init
+		void InitBuffers(bool asVelocity){
+			positionBufferDist = Buffer.CreateSquereRTBuffer(size_x, size_y);
+			positionBufferSource = Buffer.CreateSquereRTBuffer(size_x, size_y);
+			if(!asVelocity) return;
+			velocityBufferDist = Buffer.CreateSquereRTBuffer(size_x, size_y);
+			velocityBufferSource = Buffer.CreateSquereRTBuffer(size_x, size_y);
+		}
+
+		void InitMaterials(){
+			_kernelMat = MaterialFuncs.CreateMaterial(_kernel);
+			_surfaceMat = MaterialFuncs.CreateMaterial(_surface);
+
+			Graphics.Blit(null, positionBufferDist, _kernelMat, (int)KernelPass.InitPosition);
+			Graphics.Blit(null, velocityBufferDist, _kernelMat, (int)KernelPass.InitVelocity);
+		}
+
+		void InitMesh(){
+
+			_mesh = new Mesh();
+			
+			Vector3[] vertices = new Vector3[size_x * size_y];
+			int[] indices = new int[size_x * size_y];
+			for(int i = 0; i < size_x; i++){
+				for(int j = 0; j < size_y; j++){
+					vertices[i * size_y + j] = new Vector3((float)i / (float)size_x + 0.5f, (float)j / (float)size_y + 0.5f, 1.0f);
+					indices[i * size_y + j] = i * size_y + j;
+				}
+			}
+
+			_mesh.vertices = vertices;
+			_mesh.SetIndices(indices, MeshTopology.Points, 0);
+		}
+		#endregion
 	}
-	#endregion
 }
--- Assets/Shaders/NoiseKernel.shader
+++ Assets/Shaders/NoiseKernel.shader
@@ -4,68 +4,114 @@
     {
        _PositionTex ("-", 2D) = ""{}
        _VelocityTex ("-", 2D) = ""{}
-       
-       //_DeltaTime   ("-", Float) = 0
-	   _Z           ("-", Float) = 0
-       //_PointNum    ("-", int) = 0
-	   //_NoiseMode   ("-", int) = 0
-	   _AsVelocity  ("-", int) = 0
     }
 
     CGINCLUDE
 
+    #pragma multi_compile CNOISE PNOISE SNOISE SNOISE_AGRAD SNOISE_NGRAD
+    #pragma multi_compile _ THREED
+    #pragma multi_compile _ ASVELOCITY
+
     #include "UnityCG.cginc"
-	#include "SimplexNoiseGrad3D.cginc"
-    //#include "ClassicNoise3D.cginc"
-   
+
+    #if  defined(SNOISE) || defined(SNOISE_NGRAD)
+        #if defined(THREED)
+            #include "Noise/SimplexNoise3D.cginc"
+        #else
+            #include "Noise/SimplexNoise2D.cginc"
+        #endif
+    #elif defined(SNOISE_AGRAD)
+        #if defined(THREED)
+            #include "Noise/SimplexNoiseGrad3D.cginc"
+        #else
+            #include "Noise/SimplexNoiseGrad2D.cginc"
+        #endif
+    #else
+        #if defined(THREED)
+            #include "Noise/ClassicNoise3D.cginc"
+        #else
+            #include "Noise/ClassicNoise2D.cginc"
+        #endif
+    #endif
+
     sampler2D _PositionTex;
 	sampler2D _VelocityTex;
-	//float _DeltaTime;
-	float _Z;
-	//int _PointNum;
-	//int _NoiseMode;
-	int _AsVelocity;
 
 	float nrand(float2 uv, float salt) {
         uv += float2(salt, 0);
         return frac(sin(dot(uv, float2(12.9898, 78.233))) * 43758.5453);
     }
 
+	float4 frag_update_position(v2f_img i): SV_Target {	
+
+		#if defined(ASVELOCITY)
+            #if defined(SNOISE_AGRAD)
+                #if defined(THREED)
+                    float3 p = tex2D(_PositionTex, i.uv);
+                    p += snoise_grad(p) * unity_DeltaTime.y * 0.005;
+                    return float4(p, 1.0);
+                #else
+                    float2 p = tex2D(_PositionTex, i.uv);
+                    p += snoise_grad(p) * unity_DeltaTime.y * 0.005;
+                    return float4(p, 1.0, 1.0);
+                #endif
+            #endif
+		#else 
+            #if defined(THREED)
+                float3 p = float3(0, 0, 0);
+                float3 rep = float3(2.0, 2.0, 2.0);
+                float3 coord = float3(i.uv * float2(10.0, 10.0), _Time.y);
+            #else
+                float2 p = float2(0, 0);
+                float2 rep = float2(rep);
+                float2 coord = float2(i.uv * float2(10.0, 10.0) + float2(_Time.y, _Time.y));
+            #endif
+
+            #if defined(CNOISE)
+                p += cnoise(coord);
+            #elif defined(PNOISE)
+                p += pnoise(coord, rep); 
+            #elif defined(SNOISE)
+                p += snoise(coord);
+            #elif defined(SNOISE_AGRAD)
+                p += snoise_grad(coord);
+            #else //#if defined(SNOISE_NGRAD)
+                #if defined(THREED)
+                    float epsilon = 0.0001;
+                    float v0 = snoise(coord);
+                    float vx = snoise(coord + float3(epsilon, 0, 0));
+                    float vy = snoise(coord + float3(0, epsilon, 0));
+                    float vz = snoise(coord + float3(0, 0, epsilon));
+                    p += 0.5 * float3(vx - v0, vy - v0, vz - v0) / epsilon;     
+                #else
+                    float epsilon = 0.0001;
+                    float v0 = snoise(coord);
+                    float vx = snoise(coord + float2(epsilon, 0));
+                    float vy = snoise(coord + float2(0, epsilon));
+                    p += 0.5 * float2(vx - v0, vy - v0) / epsilon;      
+                #endif
+            #endif
+
+            #if defined(THREED)
+                return float4(p, 1.0);
+            #else
+                return float4(p, 1.0, 1.0);
+            #endif
+		#endif
+        return float4(1.0, 1.0, 1.0, 1.0);
+	}
+
+    float4 frag_update_velocity(v2f_img i) : SV_Target {
+		return float4(0, 0, 0, 0);
+	}
 
 	float4 frag_init_position(v2f_img i): SV_Target {
 		return float4(0,0,0,1.0);
 	}
 
-	float4 frag_update_position(v2f_img i): SV_Target {	
-		if(_AsVelocity == 0){
-
-			//float3 p = tex2D(_PositionTex, i.uv).xyz;
-            //float3 p;
-            //p.xz = i.uv - float2(0.75, 0.75);     
-            //p.y = cnoise(float3(i.uv, _Time.y));
-            float3 p =snoise_grad(float3(i.uv * float2(10.0, 10.0), _Time.y));
-            //TODO noise return 3d grad
-			return float4(p, 1.0); 
-		}else{
-
-		}
-		
-		return float4(1.0,1.0,1.0,1.0);
-	}
-
 	float4 frag_init_velocity(v2f_img i): SV_Target {		
 		return float4(0,0,0,1.0);
 	}
-
-	float4 frag_update_velocity(v2f_img i) : SV_Target {
-
-		//float p = tex2D(_PhaseTex, i.uv).x;
-		//float nf = tex2D(_NaturalFreqTex, i.uv).x;
-		//float v = nf + _K * _ParamR * sin(_ParamTheta - p);
-
-		return float4(0, 0, 0, 0);
-	}
-
     ENDCG
 
     SubShader
